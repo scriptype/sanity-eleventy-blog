e1c20a9d8d4c18f608988d67a13559f74727a52a
scriptype <scriptyper@gmail.com>
2020-07-14 19:48:40 +0300

    Give up from updating category schema

--- studio/schemas/documents/category.js
+++ studio/schemas/documents/category.js
@@ -12,16 +12,6 @@ export default {
       name: 'description',
       type: 'text',
       title: 'Description'
-    },
-    {
-      name: 'slug',
-      type: 'slug',
-      title: 'Slug',
-      description: 'Slugs are good',
-      options: {
-        source: 'name',
-        maxLength: 96
-      }
     }
   ]
 }
